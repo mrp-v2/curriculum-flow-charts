dd14f0758fda2f9d3c2490eaac81d3bc59789e8a
mrp-v2 <matthrewp@gmail.com>
2024-02-23 12:15:33 -0700

    improved dependent tree drawing to include topic dependencies

--- specific_event_dependency_chart.py
+++ specific_event_dependency_chart.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from util import DependencyInfo, Event, qualify
 
 from graphviz import Digraph
@@ -86,13 +88,8 @@ def dependents_only(info: DependencyInfo, filename_out: str, focus_event: Event)
     # main graph
     graph = Digraph(filename_out)
     graph.attr(label=f'{focus_event.unit}, {focus_event.name} Dependencies')
-    # map of each event to its subgraph
-    event_graphs: dict[Event, Digraph] = {}
-    # the focus event's subgraph for required topics
-    event_required_graph = Digraph(focus_event.name)
-    event_required_graph.attr(cluster='True')
-    event_required_graph.attr(label=focus_event.name)
-    event_graphs[focus_event] = event_required_graph
+    # map of each event to its sub-graphs of required and taught topics
+    event_graphs: dict[Event, tuple[Digraph | None, Digraph | None]] = {}
     # keep track of what's been drawn, so we don't re-draw it
     nodes_drawn: list[str] = []
     edges_drawn: list[tuple[str, str]] = []
@@ -102,17 +99,48 @@ def dependents_only(info: DependencyInfo, filename_out: str, focus_event: Event)
             graph.edge(qualified_tail, qualified_head)
             edges_drawn.append((qualified_tail, qualified_head))
 
-    def draw_topic_helper(topic: str, event: Event) -> tuple[str, str, Event]:
+    def draw_topic_only(topic: str, event: Event, default_side: Literal['taught', 'required'] = None) -> str:
         qualified_name = qualify(topic, event)
-        if event not in event_graphs:
-            temp = Digraph(event.name)
-            temp.attr(cluster='True')
-            temp.attr(label=event.name)
-            event_graphs[event] = temp
         if qualified_name not in nodes_drawn:
-            sub_graph = event_graphs[event]
-            sub_graph.node(qualified_name, topic)
-            nodes_drawn.append(qualified_name)
+            if event not in event_graphs:
+                event_graphs[event] = (None, None)
+            required, taught = event_graphs[event]
+
+            def draw_taught():
+                if taught is None:
+                    temp = Digraph(f'{event.name}$taught')
+                    temp.attr(cluster='True')
+                    event_graphs[event] = (required, temp)
+                _, sub_graph = event_graphs[event]
+                sub_graph.node(qualified_name, topic)
+                nodes_drawn.append(qualified_name)
+
+            def draw_required():
+                if required is None:
+                    temp = Digraph(f'{event.name}$required')
+                    temp.attr(cluster='True')
+                    event_graphs[event] = (temp, taught)
+                sub_graph, _ = event_graphs[event]
+                sub_graph.node(qualified_name, topic)
+                nodes_drawn.append(qualified_name)
+
+            if topic in event.topics_taught and topic in event.topics_required:
+                if default_side == 'taught':
+                    draw_taught()
+                elif default_side == 'required':
+                    draw_required()
+                else:
+                    raise ValueError('')  # TODO message
+            elif topic in event.topics_taught:
+                draw_taught()
+            elif topic in event.topics_required:
+                draw_required()
+            else:
+                raise ValueError('Given topic and event aren\'t related!')
+        return qualified_name
+
+    def draw_topic_helper(topic: str, event: Event) -> tuple[str, str, Event]:
+        qualified_name = draw_topic_only(topic, event)
         previous_taught_time = info.get_most_recent_taught_time(event, topic)
         if previous_taught_time is not None and previous_taught_time != event:
             recent_qualified_name, first_qualified_name, first_event = draw_topic_helper(topic, previous_taught_time)
@@ -152,13 +180,55 @@ def dependents_only(info: DependencyInfo, filename_out: str, focus_event: Event)
         for dependency in info.topics[topic].dependencies:
             add_topic(dependency, parent_event, dependency is not topic, qualify(topic, parent_event))
 
-    # add all the topics taught by the event
+    def add_dependent_tree(event: Event):
+        for topic in event.topics_taught:
+            if topic in topics_of_interest:
+                draw_topic_only(topic, event, 'taught')
+                draw_edge(qualify(topic, topics_of_interest[topic]), qualify(topic, event))
+                topics_of_interest[topic] = event
+            to_add: list[tuple[str, Event]] = []
+            for topic_of_interest in topics_of_interest:
+                if topic_of_interest in info.topics[topic].dependencies:
+                    draw_topic_only(topic, event, 'taught')
+                    last_taught_time = info.get_most_recent_taught_time(event, topic_of_interest, True)
+                    draw_edge(qualify(topic_of_interest, last_taught_time), qualify(topic, event))
+                    to_add.append((topic, event))
+            for topic_to_set, event_to_set in to_add:
+                topics_of_interest[topic_to_set] = event_to_set
+        for topic in event.topics_required:
+            if topic in topics_of_interest:
+                draw_topic_only(topic, event, 'required')
+                draw_edge(qualify(topic, topics_of_interest[topic]), qualify(topic, event))
+                topics_of_interest[topic] = event
+        if event.next:
+            add_dependent_tree(event.next)
+
+    # tracks topics we are interested in and the last time they appeared
+    topics_of_interest: dict[str, Event] = {}
     for taught_topic in focus_event.topics_taught:
         add_topic(taught_topic, focus_event)
-    # TODO add dependents of the event. both topic dependents (grouped by event) and event dependents.
+        topics_of_interest[taught_topic] = focus_event
+    add_dependent_tree(focus_event.next)
     # finish by adding all the sub-graphs to the main graph
-    for cluster_graph in event_graphs.values():
-        graph.subgraph(cluster_graph)
+    for event in event_graphs:
+        required_graph, taught_graph = event_graphs[event]
+        if required_graph is not None and taught_graph is not None:
+            required_graph.attr(label='Required')
+            taught_graph.attr(label='Taught')
+            sub_graph = Digraph(event.name)
+            sub_graph.attr(cluster='True')
+            sub_graph.attr(label=event.name)
+            sub_graph.subgraph(required_graph)
+            sub_graph.subgraph(taught_graph)
+            graph.subgraph(sub_graph)
+        elif required_graph is not None:
+            required_graph.attr(label=event.name)
+            graph.subgraph(required_graph)
+        elif taught_graph is not None:
+            taught_graph.attr(label=event.name)
+            graph.subgraph(taught_graph)
+        else:
+            raise RuntimeError('Event in event_graphs has no sub-graphs!')
     graph.view()
 
 
